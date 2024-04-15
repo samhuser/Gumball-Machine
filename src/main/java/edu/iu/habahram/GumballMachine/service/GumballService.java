966603f74d5cd4f0304d5b8fce239f72c4a8f670
coldapollo8 <63075497+coldapollo8@users.noreply.github.com>
2024-04-14 21:36:07 -0400

    submit

--- src/main/java/edu/iu/habahram/GumballMachine/service/GumballService.java
+++ src/main/java/edu/iu/habahram/GumballMachine/service/GumballService.java
@@ -21,9 +21,36 @@ public class GumballService implements IGumballService{
 
     @Override
     public TransitionResult insertQuarter(String id) throws IOException {
+        return changeState(id,"insertQuarter");
+    }
+
+    @Override
+    public TransitionResult ejectQuarter(String id) throws IOException {
+        return changeState(id,"ejectQuarter");
+    }
+
+    @Override
+    public TransitionResult turnCrank(String id) throws IOException {
+        return changeState(id,"turnCrank");
+    }
+
+    public TransitionResult changeState(String id, String state)throws IOException{
         GumballMachineRecord record = gumballRepository.findById(id);
         IGumballMachine machine = new GumballMachine(record.getId(), record.getState(), record.getCount());
-        TransitionResult result = machine.insertQuarter();
+        TransitionResult result;
+        switch (state){
+            case "turnCrank":
+                result = machine.turnCrank();
+                break;
+            case "ejectQuarter":
+                result = machine.ejectQuarter();
+                break;
+            case "insertQuarter":
+                result = machine.insertQuarter();
+                break;
+            default:
+                result = null;
+        }
         if(result.succeeded()) {
             record.setState(result.stateAfter());
             record.setCount(result.countAfter());
@@ -31,18 +58,6 @@ public class GumballService implements IGumballService{
         }
         return result;
     }
-
-    @Override
-    public TransitionResult ejectQuarter(String id) {
-        return null;
-    }
-
-    @Override
-    public TransitionResult turnCrank(String id) {
-        return null;
-    }
-
-    
 
     @Override
     public List<GumballMachineRecord> findAll() throws IOException {
